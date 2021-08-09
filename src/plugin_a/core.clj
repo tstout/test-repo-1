3d3347874c081a7a28cd0f7c26fc616bc9253207
Todd Stout <todd.tstout@gmail.com>
2021-08-09 18:23:04 -0500

    init plugin loader test

--- src/plugin_a/core.clj
+++ src/plugin_a/core.clj
@@ -1,4 +1,8 @@
 (ns plugin-a.core
   (:require [clojure.tools.cli :refer [parse-opts]]))
 
+(defn init []
+  (println "------>>>plugin-a loaded<<<--------"))
+
+
 (defn -main [& args] (println "hello world"))
