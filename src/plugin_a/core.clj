bf62d8b71add23d14f735b5021a326eeb37b05c8
Todd Stout <todd.tstout@gmail.com>
2021-08-22 09:25:35 -0500

    added logging dep

--- src/plugin_a/core.clj
+++ src/plugin_a/core.clj
@@ -1,8 +1,9 @@
 (ns plugin-a.core
-  (:require [clojure.tools.cli :refer [parse-opts]]))
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [taoensso.timbre :as log]))
 
 (defn init []
-  (println "------>>>plugin-a loaded<<<--------"))
+  (log/info "------>>>plugin-a loaded<<<--------"))
 
 
 (defn -main [& args] (println "hello world"))
